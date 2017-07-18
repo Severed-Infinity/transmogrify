550822be85df50dae93f8b23e5bfb94477599306
David Swift <Severed-Infinity@users.noreply.github.com>
2017-07-18 19:18:16 +0100

    cleaned up and made current tests more expressive
    add test and target paths to project.clj

--- test/transmogrify/specs/css_spec_test.clj
+++ test/transmogrify/specs/css_spec_test.clj
@@ -7,22 +7,25 @@
 
 (test/deftest css-spec-property-tests
   (test/testing "css font specs"
-    (test/testing "css font family spec"
+    (test/testing "-> font family spec"
+      ;; TODO Sub specs
+      ;; FIXME proper output comparison
       (test/is (not= ::s/invalid (s/conform ::css-spec/font-family ["helvetica"])) "coll of a single font as a string")
       (test/is (not= ::s/invalid (s/conform ::css-spec/font-family [:serif])) "coll of a single font from generics")
       (test/is (not= ::s/invalid (s/conform ::css-spec/font-family ["helvetica" :serif])) "coll of multiple mixed font options")
       (test/is (= ::s/invalid (s/conform ::css-spec/font-family "helvetica")) "string font should be invalid")
       (test/is (= ::s/invalid (s/conform ::css-spec/font-family :serif)) "keyword generic font should be invalid"))
 
-    (test/testing "css font weight spec"
-      (test/testing "testing css-spec/multiple-of-100?"
-        (test/is (true? (css-spec/multiple-of-100? 800)))
-        (test/is (true? (css-spec/multiple-of-100? 300)))
-        (test/is (false? (css-spec/multiple-of-100? 90)))
-        (test/is (false? (css-spec/multiple-of-100? 1110)))
-        (test/is (false? (css-spec/multiple-of-100? -330)))))
+    (test/testing "-> font weight spec"
+      (test/testing "-> testing css-spec/multiple-of-100?"
+        (test/is (true? (css-spec/multiple-of-100? 800)) "positive 100's, should be true")
+        (test/is (true? (css-spec/multiple-of-100? 300)) "positive 100's, should be true")
+        (test/is (true? (css-spec/multiple-of-100? -200)) "negative 100's should be true")
+        (test/is (false? (css-spec/multiple-of-100? 90)) "not a multiple of 100, should be false")
+        (test/is (false? (css-spec/multiple-of-100? 1110)) "not a multiple of 100, should be false")
+        (test/is (false? (css-spec/multiple-of-100? -330)))) "not a multiple of 100, should be false")
 
-    (test/testing "css properties map of just font specs"
+    (test/testing "-> properties map of just font specs"
       (test/is
         (not=
           ::s/invalid
