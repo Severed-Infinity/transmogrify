31c00e03e9acff24d1fde1299619c8ee5cbcc93b
David Swift <Severed-Infinity@users.noreply.github.com>
2017-10-24 23:17:14 +0100

    updated dependencies, removed cloverage temporary

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
                 
                 Given a nested (vector) datastructure transform it into the given language provided it 
                 matches the language spec provided and the transformation algorithm"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.854"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/spec.alpha "0.1.123"]
-                 [org.clojure/core.specs.alpha "0.1.10"]
-                 [metosin/spec-tools "0.3.1"]]
+                 [org.clojure/core.specs.alpha "0.1.24"]
+                 [metosin/spec-tools "0.3.3"]]
   :plugins [[lein-cloverage "1.0.9"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [cloverage/cloverage "1.0.9"]]}}
